0cfd17d3620de3d3cf6cb256bab95229727a7fdf
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-12-03 16:50:51 -0300

    FIX: fixes settings to run app

--- Taskify/Program.cs
+++ Taskify/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -14,7 +15,7 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.UseSwagger();
-    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
