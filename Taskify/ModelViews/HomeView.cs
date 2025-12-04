a4fe473bb28fbbff9387a890e86962e33116be20
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-12-04 17:01:55 -0300

    FEAT: adds visualization class

---
+++ Taskify/ModelViews/HomeView.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taskify.ModelViews
+{
+    public class HomeView
+    {
+        public required string Message { get; set; }
+        public required string Documentation { get; set; }
+    }
+}
