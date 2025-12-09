7ed99840608b5170e392465d3ca7676a0339838c
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-12-09 17:05:58 -0300

    FEAT: adds attributes to properties

--- Taskify/Models/Task.cs
+++ Taskify/Models/Task.cs
@@ -1,14 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Taskify.Models
 {
+    [Table("tasks")]
     public class Task
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Title { get; set; } = default!;
+
+        [Column(TypeName = "text")]
         public string Description { get; set; } = default!;
         public int Priority { get; set; }
         public bool Finished { get; set; }
