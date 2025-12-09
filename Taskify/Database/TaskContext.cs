46bd6a9f5dd7830244938a0b0f50457a7cc75881
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-12-09 16:58:36 -0300

    FEAT: adds Context class

---
+++ Taskify/Database/TaskContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Taskify.Database
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
