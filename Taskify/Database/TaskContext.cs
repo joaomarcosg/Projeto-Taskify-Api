41788be78e535ec075776d28189550374e6f3824
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-12-11 16:41:22 -0300

    FIX: removes wrong import

--- Taskify/Database/TaskContext.cs
+++ Taskify/Database/TaskContext.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Taskify.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Taskify.Database
